ef58344fc216355ba6e6b5e450dc4fc6f8d4afec
V5777 (Course ID) <v5777@cam.ac.uk>
2015-08-14 15:17:53 +0100

    2nd attempt

--- drone-backend.js
+++ drone-backend.js
@@ -42,7 +42,7 @@ function fly(robot) {
         bot.drone.forward(0.2);
     });
 
-    after(11*1000, function() {
+    after(11*1000, function() { //stop
         bot.drone.forward(0);
     });
 
@@ -50,16 +50,51 @@ function fly(robot) {
         bot.drone.back(0.2);
     });
 
-    after(15*1000, function() {
+    after(15*1000, function() {//stop
         bot.drone.back(0);
     });
 
-    after(16*1000, function() {
+    for(var i=0; i<4;i++){
+        after((15.1+4*i)*1000, function() {
+            bot.drone.right(0.2);
+        });
+
+        after((17+4*i)*1000, function() { //stop
+            bot.drone.right(0);
+        });
+        after((17.1+4*i)*1000, function() {
+            bot.drone.left(0.2);
+        });
+
+        after((19 + 4 * i)*1000, function() {//stop
+            bot.drone.left(0);
+        });
+    }
+
+    after(31.1*1000, function(){
+       bot.drone.clockwise(0.2)
+    });
+
+    after(34*1000, function(){
+       bot.drone.clockwise(0.2)
+    });
+
+    after(31.1*1000, function(){
+      bot.drone.anticlockwise(0.2)
+    });
+
+    after(34*1000, function(){
+       bot.drone.anticlockwise(0.2)
+     });
+
+            after(34.1*1000, function() {
         bot.drone.land();
     });
     after(21*1000, function() {
         bot.drone.stop();
     });
+
+
 }
 
 Cylon.start();
