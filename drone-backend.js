3ff0facc3cfc6f5fb933403b5a3458fd6c64730a
V5777 (Course ID) <v5777@cam.ac.uk>
2015-08-14 11:55:41 +0100

    1st attempt

--- drone-backend.js
+++ drone-backend.js
@@ -15,7 +15,51 @@ Cylon.robot()
     
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
 
+    /*bot.nav.on("navdata", function(data) {
+        // console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });*/
+
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+
+    bot.drone.takeoff();
+
+    after(2*1000, function() {
+        bot.drone.forward(0.2);
+    });
+
+    after(5*1000, function() {
+        bot.drone.forward(0);
+    });
+
+    after(6*1000, function() {
+        bot.drone.back(0.2);
+    });
+
+    after(9*1000, function() {
+        bot.drone.back(0);
+    });
+
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+    after(15*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
